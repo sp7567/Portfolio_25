5f08b63a9fd8d9c9137cf2ba5360632cd43f5882
sp7567 <shrayaspadase7567@gmail.com>
2025-04-13 19:55:04 +0530

    Initial Node.js app

--- assets/js/script.js
+++ assets/js/script.js
@@ -68,7 +68,7 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Jigar Sable";
+            document.title = "Portfolio | Shreyash Padase";
             $("#favicon").attr("href", "assets/images/favicon.png");
         }
         else {
